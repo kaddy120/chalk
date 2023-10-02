f4d51f4250a0b5d5a247796f86cf1b3041e81efc
kaddy120 <kaddy120@gmail.com>
2023-10-02 23:21:13 +0200

    add a Bio component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
-import './App.css';
-import Button from 'react-bootstrap/Button'
+import { useState } from 'react';
+import BioForm from './BioForm';
+import Page from './Page';
+// import BioForm from './BioForm'
 
 // or, specify which plugins you need:
-const Page = () => {
-  return (
-    <div>
-      <div className='container py-4 px-3 mx-auto component'>
-        <h1>Hello, Bootstrap and Vite!</h1>
-        <Button variant='primary'>Primary button</Button>
-      </div>
-    </div>
-  );
-};
 
 function App() {
+  let bioDetails = {
+    name: 'Kaddy Marindi',
+    location: 'South Africa, Limpop',
+    contact: '(+27)799895113',
+    email: 'kaddy120@gmail.com',
+    url: 'kaddy120.github.io',
+    githubUsername: 'kaddy120',
+    linkedIn: 'linked.com/kad.marindi',
+    twitter: 'twitter.com/kaddy120',
+  };
+
+  const [bio, setBio] = useState(bioDetails);
+  const [aboutMe, setAboutMe] = useState(
+    'Aspiring software developer, who is passionate about front-end development'
+  );
+
+  function handleChange(e) {
+    setBio({
+      ...bio,
+      [e.target.name]: e.target.value,
+    });
+  }
+
   return (
     <>
-      <Page />
+      <div className='container flex'>
+        <BioForm bio={bio} handleChange={handleChange} aboutMe={aboutMe} setAboutMe={setAboutMe}/>
+        <Page aboutMe={aboutMe} bio={bio}/>
+      </div>
     </>
   );
 }
